c73977fced681cd90b5b269f66544ecea2530b82
sargdsra <sargdsra@gmail.com>
2021-04-26 14:31:40 +0430

    Initial files added.

---
+++ dataloader.py
@@ -0,0 +1,59 @@
+import random
+import torchvision
+from torch.utils.data import Dataset
+from PIL import Image
+
+
+class RotationLoader(Dataset):
+    def __init__(self, root_dir, num_angles):
+        super(RotationLoader, self).__init__()
+        self.root_dir = root_dir
+        file_ind = open(self.root_dir)
+        self.data = [line.rstrip() for line in file_ind.readlines()]
+        file_ind.close()
+        self.color_transform = torchvision.transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.4)
+        # self.flips = [torchvision.transforms.RandomHorizontalFlip(), torchvision.transforms.RandomVerticalFlip()]
+        self.normalize = torchvision.transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.orig_size = 300
+        self.crop_size = 224
+        self.num_angles = num_angles
+        self.angle_ratio = 360 // num_angles
+        self.angles = [i * self.angle_ratio for i in range(self.num_angles)]
+        
+    def get_image(self, path):
+        image = Image.open(path).convert('RGB')
+        return image
+    
+    def __len__(self):
+        return len(self.data)
+       
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        """
+        original = self.get_image(self.data[index])
+        image = torchvision.transforms.Resize((self.orig_size, self.orig_size))(original)
+        image_part = torchvision.transforms.RandomCrop((self.res_crop_size, self.res_crop_size))(image_part)
+        rotation = torchvision.transforms.Resize((self.res_crop_size, self.res_crop_size))(image_part)
+        # augmentation - collor jitter
+        # image = self.color_transform(image)
+        rotation = self.color_transform(rotation)
+        # augmentation - flips
+        # image = self.flips[0](image)
+        # image = self.flips[1](image)
+        # augmentation - rotation
+        angle = random.choice(self.angles)
+        angle_index = self.angles.index(angle)
+        rotation = torchvision.transforms.functional.rotate(rotation, angle)
+
+        # to tensor
+        # image = torchvision.transforms.functional.to_tensor(image)
+        rotation = torchvision.transforms.functional.to_tensor(rotation)
+        # normalize
+        # image = self.normalize(image)
+        rotation = self.normalize(rotation)
+
+        return {'rotation': rotation, 'angle_index': angle_index}
---
+++ utils.py
@@ -0,0 +1,15 @@
+import torch
+
+
+def save_checkpoint(epoch, model, optimizer, filename):
+    """
+    Save model checkpoint.
+
+    :param epoch: epoch number
+    :param model: model
+    :param optimizer: optimizer
+    """
+    state = {'epoch': epoch,
+             'model': model,
+             'optimizer': optimizer}
+    torch.save(state, filename)
